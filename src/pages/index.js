218d59ffa72c799185babd0cbabf68d08edd6236
GrafSoul <grafsoul@gmail.com>
2021-04-19 13:36:50 +0500

    feat: Added code components and links.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
+// Core
 import React from 'react';
+// Components
+import Header from '../components/header';
+// Router
+import { Link } from 'gatsby';
 
 export default function Home() {
-    return <div>Hello world! I am Gatsby!</div>;
+    return (
+        <div style={{ color: 'teal' }}>
+            <Link to="/contact/">Contact</Link>
+            <Header headerText="About Gatsby" />
+            <p>Such wow. Very React.</p>
+            <img src="https://source.unsplash.com/random/400x200" alt="" />
+        </div>
+    );
 }
