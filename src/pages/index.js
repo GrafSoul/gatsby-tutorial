135e26c29d5be5b3ce0bc25b1f21467830b70a2e
GrafSoul <grafsoul@gmail.com>
2021-04-19 20:28:31 +0500

    feat: Added nested layout components.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,21 @@
 import React from 'react';
 // Components
 import Header from '../components/header';
-// Router
-import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default function Home() {
     return (
-        <div style={{ color: 'teal' }}>
-            <Link to="/contact/">Contact</Link>
+        <Layout>
             <Header headerText="About Gatsby" />
+            <h1>
+                Hi! I&apos;m building a fake Gatsby site as part of a tutorial!
+            </h1>
+            <p>
+                What do I like to do? Lots of course but definitely enjoy
+                building websites.
+            </p>
             <p>Such wow. Very React.</p>
             <img src="https://source.unsplash.com/random/400x200" alt="" />
-        </div>
+        </Layout>
     );
 }
