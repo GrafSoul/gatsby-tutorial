9630681f182a87ddf869b7ed4fcd76ec24610317
GrafSoul <grafsoul@gmail.com>
2021-04-19 20:31:15 +0500

    feat: Deleted the unnecessary text.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,16 +7,14 @@ import Layout from '../components/layout';
 export default function Home() {
     return (
         <Layout>
-            <Header headerText="About Gatsby" />
             <h1>
                 Hi! I&apos;m building a fake Gatsby site as part of a tutorial!
             </h1>
+            <img src="https://source.unsplash.com/random/400x200" alt="" />
             <p>
                 What do I like to do? Lots of course but definitely enjoy
                 building websites.
             </p>
-            <p>Such wow. Very React.</p>
-            <img src="https://source.unsplash.com/random/400x200" alt="" />
         </Layout>
     );
 }
